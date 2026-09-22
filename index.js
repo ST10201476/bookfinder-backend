3b27cad43bccd5f30b2bdb62bf18a24704e55796
Sandile <ST10201476@rcconnect.edu.za>
2026-09-22 13:00:10 +0200

    Support environment variable for Firebase credentials

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require("./serviceAccountKey.json");
 
 initializeApp({
     credential: cert(serviceAccount)
